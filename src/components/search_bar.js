c0f66e17005072aa76143a6bab9929e1cab432db
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-05-22 14:01:10 -0500

    15: Class-based components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // component purpose = takes user input & makes API request
+// functional component vs. class-based component
 
-const SearchBar = () => {
-  return <input/>
-};
+// FUNCTIONAL COMPONENT
+// --the only thing that comes out is JSX
+// const SearchBar = () => {
+//   return <input/>
+// };
+
+// CLASS-BASED COMPONENT
+// used whenever we want a component to have any sort of internal record keeping
+// because users will be typing into this component, it needs the ability to introspect a little
+// gives it access to all of the React.Component functionality
+// every component that is class-based must have a render method
+// class SearchBar extends React.Component {
+//   render() {
+//     return <input/>;
+//   }
+// }
+
+// refactored by adding ', { Component }' at top
+class SearchBar extends Component {
+  render() {
+    return <input/>;
+  }
+}
 
 export default SearchBar;
