37be7ae5ef3a0f9088ea37145a42833344f7c8d8
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-05-22 13:31:31 -0500

    10: Component Structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
+// used to create and render components
 import React from 'react';
 
-// 1) Create a new component, should produce some html
+// used to react with the DOM
+import ReactDOM from 'react-dom';
+
+// 1) Create a new COMPONENT, component should produce some html
 
 // const = ES6 syntax = final value of variable, aka, constant at this point, we're never going to reassign 'App'
 
 // JSX = subset of JS, to write what looks like html
+// babel translates this to vanilla JS/actual html
+// render puts it on the page
 
-const App = function() {
+// What is React? JS library to is used to produce html that is shown to the user in the web browser.
+
+// Component = VIEW = snippet of code that produces html, collection of JS objects
+
+// React code = multiple components nested
+
+// this is a class, not instance
+// factory that creates instances
+// create instance by making an App tag (<App />)
+
+// const App = function() {
+//   return <div>Hi!</div>;
+// }
+
+// => fat arrow is the same as 'function'
+const App = () => {
   return <div>Hi!</div>;
 }
 
+// App = class
+// <App/> = instance of class
+
 // 2) Take component's generated HTML & put on page (in DOM)
-React.render(App);
+// 2nd argument is a target DOM element
+ReactDOM.render(<App/>, document.querySelector('.container'));
