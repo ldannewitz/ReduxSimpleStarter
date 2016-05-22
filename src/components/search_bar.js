47327d46e5909e7a311e0865ff2d9c37f5d4715c
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-05-22 14:44:41 -0500

    16: Handling user events

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,7 @@ import React, { Component } from 'react';
 
 // CLASS-BASED COMPONENT
 // used whenever we want a component to have any sort of internal record keeping
-// because users will be typing into this component, it needs the ability to introspect a little
+// because users will be typing into this component, it needs the ability to introspect a little (ability to detect events)
 // gives it access to all of the React.Component functionality
 // every component that is class-based must have a render method
 // class SearchBar extends React.Component {
@@ -20,11 +20,22 @@ import React, { Component } from 'react';
 //   }
 // }
 
+// handling events
+// --1) declare event handler; 2)
+// 'onChange' = React event, look to React docs for others
+
 // refactored by adding ', { Component }' at top
 class SearchBar extends Component {
   render() {
-    return <input/>;
+    // return <input onChange={this.onInputChange} />;
+    // can also drop the parens around event 
+    return <input onChange={(event) => console.log(event.target.value)} />;
   }
+
+  // onInputChange(event) {
+  //   // console.log(event);
+  //   console.log(event.target.value);
+  // }
 }
 
 export default SearchBar;
