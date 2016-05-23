8d76a1a5730fee385770494662df769670229f5b
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-05-22 19:35:22 -0500

    End of Section 1: An Intro to React

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,41 +1,21 @@
 import React, { Component } from 'react';
 
-// component purpose = takes user input & makes API request
-// functional component vs. class-based component
-
-// FUNCTIONAL COMPONENT
-// --the only thing that comes out is JSX
-// const SearchBar = () => {
-//   return <input/>
-// };
-
-// CLASS-BASED COMPONENT
-// used whenever we want a component to have any sort of internal record keeping
-// because users will be typing into this component, it needs the ability to introspect a little (ability to detect events)
-// gives it access to all of the React.Component functionality
-// every component that is class-based must have a render method
-// class SearchBar extends React.Component {
-//   render() {
-//     return <input/>;
-//   }
-// }
-
-// handling events
-// --1) declare event handler; 2)
-// 'onChange' = React event, look to React docs for others
-
-// refactored by adding ', { Component }' at top
 class SearchBar extends Component {
-  render() {
-    // return <input onChange={this.onInputChange} />;
-    // can also drop the parens around event 
-    return <input onChange={(event) => console.log(event.target.value)} />;
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: '' };
   }
 
-  // onInputChange(event) {
-  //   // console.log(event);
-  //   console.log(event.target.value);
-  // }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.searchTerm}
+          onChange={event => this.setState({ searchTerm: event.target.value })} />
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
-// used to create and render components
 import React from 'react';
-
-// used to react with the DOM
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/search_bar';
 
-// 1) Create a new COMPONENT, component should produce some html
-
-// const = ES6 syntax = final value of variable, aka, constant at this point, we're never going to reassign 'App'
-// no longer use 'var', use 'const' or 'let'
 const API_KEY = 'AIzaSyAKaiQaodB8LDgZ6RMgaSX8jR8-8pAgsGk';
 
-// JSX = subset of JS, to write what looks like html
-// babel translates this to vanilla JS/actual html
-// render puts it on the page
-
-// What is React? JS library to is used to produce html that is shown to the user in the web browser.
-
-// Component = VIEW = snippet of code that produces html, collection of JS objects
-
-// React code = multiple components nested
-
-// this is a class, not instance
-// factory that creates instances
-// create instance by making an App tag (<App />)
-
-// const App = function() {
-//   return <div>Hi!</div>;
-// }
-
-// => fat arrow is the same as 'function'
 const App = () => {
   return (
     <div>
@@ -39,9 +12,4 @@ const App = () => {
   );
 }
 
-// App = class
-// <App/> = instance of class
-
-// 2) Take component's generated HTML & put on page (in DOM)
-// 2nd argument is a target DOM element
 ReactDOM.render(<App/>, document.querySelector('.container'));
